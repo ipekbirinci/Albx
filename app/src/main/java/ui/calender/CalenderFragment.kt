5af45658eaef07f07eb05c433e38582fb82969f5
İpek <ipek.birinci8@gmail.com>
2024-02-07 22:35:03 +0300

    Add strings,textviews,images and cardview to project.

--- app/src/main/java/ui/calender/CalenderFragment.kt
+++ app/src/main/java/ui/calender/CalenderFragment.kt
@@ -6,18 +6,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.albx.R
+import com.example.albx.databinding.FragmentMarketsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
+import data.repo.StockRepository
+import ui.markets.MarketsAdapter
+import ui.markets.MarketsViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CalenderFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CalenderFragment()
-    }
 
-    private lateinit var viewModel: CalenderViewModel
+    @Inject
+    lateinit var stockRepository: StockRepository
+    var adapter: MarketsAdapter?=null
+    private val viewModel by viewModels<MarketsViewModel>()
+    private lateinit var binding: FragmentMarketsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,10 +33,6 @@ class CalenderFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_calender, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CalenderViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
--- app/src/main/java/ui/markets/MarketsViewModel.kt
+++ app/src/main/java/ui/markets/MarketsViewModel.kt
@@ -1,10 +1,34 @@
 package ui.markets
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import data.model.Stock
+import data.model.StockResponse
+import data.repo.StockRepository
+import data.source.StockApi
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
-class MarketsViewModel @Inject constructor(): ViewModel() {
-    // TODO: Implement the ViewModel
+class MarketsViewModel @Inject constructor(private val repository: StockRepository): ViewModel() {
+
+    @Inject
+    lateinit var apiRepository: StockRepository
+
+    private var _stockLiveData = MutableLiveData<List<Stock>?>()
+    val stockLiveData: LiveData<List<Stock>?>
+        get() = _stockLiveData
+
+
+    fun getStocks() {
+        apiRepository.getAllStocks()
+    }
+
 }
